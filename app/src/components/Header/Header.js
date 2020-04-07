ad92db96ad05d405524482cbe323c1aff0d24387
stefangeneralao <stefangeneralao@gmail.com>
2020-04-07 13:07:43 +0200

    Added animation to header

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -10,6 +10,29 @@ const StyledHeader = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-end;
+  text-shadow: 3px -3px white;
+  color: ${ ({ theme }) => theme.primary };
+
+  &:hover {
+    animation: color-rotate 300ms;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+    animation-timing-function: ease-in-out;
+  }
+
+  @keyframes color-rotate {
+    0% {
+      color: ${ ({ theme }) => theme.primary };
+      text-shadow: 3px -3px white;
+    }
+    50% {
+      color: #2d67fb;
+    }
+    100% {
+      color: ${ ({ theme }) => theme.secondary };
+      text-shadow: -3px -3px white;
+    }
+  }
 `;
 
 const Img = styled.img`
@@ -24,7 +47,6 @@ const Img = styled.img`
 `;
 
 const H1 = styled.h1`
-  color: ${ ({ theme }) => theme.primary };
   font-size: 7vw;
   margin: 0;
   font-family: 'Press Start 2P', cursive;
