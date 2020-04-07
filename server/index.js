a976933fda22c7e4d11cd31618513eda5eaad60b
stefangeneralao <stefangeneralao@gmail.com>
2020-04-07 13:05:57 +0200

    Replaced mock data with DB using MongoDB

--- server/index.js
+++ server/index.js
@@ -1,37 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import { v4 as uuid } from 'uuid';
+import { MongoClient } from 'mongodb';
 
-const generateItem = value => ({
-  id: uuid(),
-  value,
-  voters: [],
-});
+if (!process.env.DB_URL) {
+  console.log('DB_URL is missing from .env, fix it! Bye.');
+  process.exit();
+}
 
-const storage = {
-  items: [{
-    id: uuid(),
-    value: '12 april 20:00',
-    voters: [ uuid(), uuid(), uuid(), uuid() ],
-  }, {
-    id: uuid(),
-    value: '12 april 21:00',
-    voters: [],
-  }, {
-    id: uuid(),
-    value: '12 april 19:00',
-    voters: [ uuid(), uuid() ]
-  }, {
-    id: uuid(),
-    value: '13 april 19:00',
-    voters: [ uuid(), uuid() ]
-  }, {
-    id: uuid(),
-    value: '13 april 20:00',
-    voters: [ uuid() ]
-  }],
-};
+const dbUrl = process.env.DB_URL;
 
 const app = express();
 
@@ -39,50 +16,87 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('*', (_, res, next) => {
-  res.on('finish', () => {
-    console.log(storage.items);
-  });
-  next();
-});
-
 app.get('/', (_, res) => {
   res.sendStatus(200);
 });
 
-app.get('/items', (_, res) => {
-  res.status(200).send(storage.items.sort((a, b) => ( b.voters.length - a.voters.length )));
-});
-
-app.post('/vote', (req, res) => {
-  const { itemId, userToken } = req.body;
-  
-  const { voters } = storage.items.find(({ id }) => id === itemId);
-  if (!voters.includes(userToken)) {
-    voters.push(userToken);
+app.get('/items', async (_, res) => {
+  try {
+    const client = await MongoClient.connect(dbUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    const collection = client.db().collection('items');
+    const items = await collection.find({}).toArray();
+    client.close();
+    res.status(200).send(items.sort((a, b) => ( b.voters.length - a.voters.length )));
+  } catch {
+    res.sendStatus(500);
   }
-
-  res.sendStatus(204);
 });
 
-app.delete('/vote', (req, res) => {
-  const { itemId, userToken } = req.body;
-  
-  const { voters } = storage.items.find(({ id }) => id === itemId);
-  if (voters.includes(userToken)) {
-    const voterIndex = voters.findIndex(voter => voter === userToken);
-    if (voterIndex !== -1) {
-      voters.splice(voterIndex, 1);
-    }
+app.post('/item', async (req, res) => {
+  const { value, _id, userToken } = req.body;
+
+  try {
+    const client = await MongoClient.connect(dbUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    const collection = client.db().collection('items');
+    await collection.insertOne({
+      _id,
+      value,
+      createdBy: userToken,
+      voters: [],
+    });
+    client.close();
+    res.status(200);
+  } catch {
+    res.sendStatus(500);
   }
-  
-  res.sendStatus(204);
 });
 
-app.post('/item', (req, res) => {
-  const { value } = req.body;
-  storage.items.push(generateItem(value));
-  res.sendStatus(501);
+app.post('/vote', async (req, res) => {
+  const { itemId, userToken } = req.body;
+  try {
+    const client = await MongoClient.connect(dbUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    const collection = client.db().collection('items');
+    const item = await collection.findOne({ _id: itemId });
+    const voters = [ ...item.voters, userToken ];
+    collection.updateOne(
+      { _id: itemId },
+      { $set: { voters } },
+    );
+    client.close();
+    res.sendStatus(204);
+  } catch {
+    res.sendStatus(500);
+  }
+});
+
+app.delete('/vote', async (req, res) => {
+  const { itemId, userToken } = req.body;
+  try {
+    const client = await MongoClient.connect(dbUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    const collection = client.db().collection('items');
+    const item = await collection.findOne({ _id: itemId });
+    const voters = [ ...item.voters ].filter(voter => voter !== userToken);
+    collection.updateOne(
+      { _id: itemId },
+      { $set: { voters } },
+    );
+    client.close();
+    res.sendStatus(204);
+  } catch {
+    res.sendStatus(500);
+  }
 });
 
 const port = process.env.PORT || 3001;
